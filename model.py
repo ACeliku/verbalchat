898359c016181c7a2423766d43633caa4036abf0
Armir Celiku <113515524+ACeliku@users.noreply.github.com>
2023-12-14 16:23:48 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,20 +1,24 @@
-# Import necessary libraries
+import requests
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain import PromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
 import chainlit as cl
-from transformers import AutoModelForCausalLM
+import asyncio
 
-# Path to the FAISS vector store
+#console command: chainlit run model2.py -w
+# Configuration Constants
 DB_FAISS_PATH = 'vectorstore/db_faiss'
+TOGETHER_AI_URL = "https://api.together.xyz/inference"
+TOGETHER_AI_MODEL = "mistralai/Mistral-7B-Instruct-v0.1"
+API_KEY = "9116a14c0461e8bb61afa5acc310a4d6f8c4a2358d1fe8c1040945106e8b5acb"
 
-# Custom prompt template for the chatbot
+# Custom Prompt Template
 custom_prompt_template = """
-Act as a medical assistant. When people say hello, also say hello back to them. Be polite and warm. Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+You are a chatbot that has access to context retrieved from a database of medical labels from the fda database of drugs. 
+You are only allowed to answer question relating to various drugs and medications, and only with responses that can be referenced back to the context provided to you.
+Make sure your responses are well structured, complete, answer the question with details, and end on a period. Be kind, when people say hello, greet them, and try to make them feel welcome.
 
 Context: {context}
 Question: {question}
@@ -23,71 +27,70 @@ Only return the helpful answer below and nothing else.
 Helpful answer:
 """
 
-# Function to set the custom prompt template
+
 def set_custom_prompt():
-    prompt = PromptTemplate(template=custom_prompt_template,
-                            input_variables=['context', 'question'])
-    return prompt
+  return PromptTemplate(template=custom_prompt_template,
+                        input_variables=['context', 'question'])
 
-# Function to create the Retrieval QA Chain
-def retrieval_qa_chain(llm, prompt, db):
-    qa_chain = RetrievalQA.from_chain_type(llm=llm,
-                                           chain_type='stuff',
-                                           retriever=db.as_retriever(search_kwargs={'k': 2}),
-                                           return_source_documents=True,
-                                           chain_type_kwargs={'prompt': prompt})
-    return qa_chain
 
-# Function to load the language model
-def load_llm():
-    llm = CTransformers(
-        model = AutoModelForCausalLM.from_pretrained("TheBloke/dolphin-2.2.1-mistral-7B-GGUF"),
-        model_type="llama",
-        max_new_tokens=2000,
-        temperature=0.9
-    )
-    return llm
+def together_ai_call(prompt):
+  payload = {
+      "model": TOGETHER_AI_MODEL,
+      "prompt": prompt,
+      "max_tokens": 2000,
+      # "stop": ".",
+      "temperature": 0.1,
+      # "top_p": 0.7,
+      # "top_k": 2,
+      # "repetition_penalty": 1
+  }
+  headers = {
+      "accept": "application/json",
+      "content-type": "application/json",
+      "Authorization": f"Bearer {API_KEY}"
+  }
+  response = requests.post(TOGETHER_AI_URL, json=payload, headers=headers)
+  return response.json()
 
-# Function to initialize the QA Bot
+
+def retrieval_qa_chain(prompt, db, embeddings):
+
+  def qa_chain(query):
+    query_vector = embeddings.embed_documents([query])[0]
+    retrieved_context = db.search(query, search_type='similarity', k=5)
+    formatted_context = ' '.join(doc.page_content for doc in retrieved_context)
+    formatted_prompt = prompt.format(context=formatted_context, question=query)
+    return together_ai_call(formatted_prompt), retrieved_context
+
+  return qa_chain
+
+
 def qa_bot():
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                       model_kwargs={'device': 'cpu'})
-    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
-    llm = load_llm()
-    qa_prompt = set_custom_prompt()
-    qa = retrieval_qa_chain(llm, qa_prompt, db)
-    return qa
+  embeddings = HuggingFaceEmbeddings(
+      model_name="sentence-transformers/all-MiniLM-L6-v2",
+      model_kwargs={'device': 'cpu'})
+  db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+  qa_prompt = set_custom_prompt()
+  return retrieval_qa_chain(qa_prompt, db, embeddings)
 
-# Function to generate the final response
-def final_result(query):
-    qa_result = qa_bot()
-    response = qa_result({'query': query})
-    return response
 
-# ChainLit code for the chatbot
 @cl.on_chat_start
 async def start():
-    chain = qa_bot()
-    msg = cl.Message(content="Starting the bot...")
-    await msg.send()
-    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
-    await msg.update()
-    cl.user_session.set("chain", chain)
+  chain = qa_bot()
+  await cl.Message(content="Hi, Welcome to Medchat. What is your query?"
+                   ).send()
+  cl.user_session.set("chain", chain)
+
 
 @cl.on_message
 async def main(message: cl.Message):
-    chain = cl.user_session.get("chain") 
-    cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
-    )
-    cb.answer_reached = True
-    res = await chain.acall(message.content, callbacks=[cb])
-    answer = res["result"]
-    sources = res["source_documents"]
+  chain = cl.user_session.get("chain")
+  api_response, sources = chain(message.content)
 
-    if sources:
-        answer += f"\nSources:" + str(sources)
-    else:
-        answer += "\nNo sources found"
+  # Extract the response text
+  response_text = api_response.get('output', {}).get('choices', [{}])[0].get(
+      'text', 'No response generated.').strip() if isinstance(
+          api_response, dict) else "API response format is not as expected."
 
-    await cl.Message(content=answer).send()
+  # Send only the response text without appending source_info
+  await cl.Message(content=response_text).send()
